2c5ef2984284f3b776f22da8cf3dc45e254594aa
wilson <wilalexport.marroquin@gmail.com>
2024-11-14 11:28:18 -0600

    Agregar archivos de proyecto.

---
+++ proyecto_final_BD-Grupo#03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_final_BD_Grupo_03
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
